89ab9ebd14c3f46ec5b0fa45a25c837a4f4aa1d3
cheongchan <cheong.chan@yale.edu>
2013-06-03 15:45:18 -0400

    can get speaker names

--- Speaker.rb
+++ Speaker.rb
@@ -1,19 +1,27 @@
 class Speaker
+attr_reader :spoken_to,:name
+@@all_speakers = []
 
-#initializes class of speaker
-	def initialize()
-		@name = 
+	def initialize(name)
+		@name = name.to_sym
+		@spoken_to = []
+		@@all_speakers << name.to_sym
 	end
 
-#returns name of speaker
-	def whoIsSpeaker
-		return @name
+	def list_speakers
+		return @@all_speakers
 	end
 
-#returns the name of the people who speaker has spoken to
-	def Speakee
-		return
-	end
-
-
+	def include?(name)
+		return list_speakers.include?(name.to_sym)
+	end	
+		
 end
+
+
+# sally = Person.new("Sally")
+# jon = Person.new("Jon")
+
+# jon.speak_to(sally)
+
+# jon.spoken_to
--- colon_names.rb
+++ colon_names.rb
@@ -1,10 +1,33 @@
+#add speaker class definition
+require 'Speaker'
+
+test = Speaker.new("test")
+
 File.open('sample_input.txt', 'r') do |f1|  
 
-	while line = f1.gets
-	puts line
-	#puts the speaker name
-    puts line[/\w+/]
-    #puts the recipient
-    puts line.scan(/\@\w+/)
+	 while line = f1.gets
+		puts line
+
+		speaker = line[/\w+/]
+		speakee	= line.scan(/\@\w+/)
+		#puts the speaker name
+	    puts speaker
+	    #puts the recipient
+	    puts speakee
+
+	    	#Checks if speaker exists
+			if test.include?(speaker)
+			puts test.list_speakers
+
+			else	
+			#if doesn't exists create new person
+			speaker = Speaker.new(speaker)
+			speaker.spoken_to << speakee
+			puts speaker.spoken_to
+			end
+	
 	end
+
+	puts test.list_speakers
+
 end
