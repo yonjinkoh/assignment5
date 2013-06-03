1f680a92e6abc53a4c06738b64763465dad429fc
cheongchan <cheong.chan@yale.edu>
2013-06-03 13:23:51 -0400

    something

---
+++ Speaker.rb
@@ -0,0 +1,19 @@
+class Speaker
+
+#initializes class of speaker
+	def initialize()
+		@name = 
+	end
+
+#returns name of speaker
+	def whoIsSpeaker
+		return @name
+	end
+
+#returns the name of the people who speaker has spoken to
+	def Speakee
+		return
+	end
+
+
+end
