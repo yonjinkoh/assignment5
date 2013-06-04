e5ba3840fddf5b8dd811c4425cf68abd3837c139
cheongchan <cheong.chan@yale.edu>
2013-06-04 09:36:13 -0400

    better

--- colon_names.rb
+++ colon_names.rb
@@ -1,21 +1,17 @@
 #add speaker class definition
 require 'Speaker'
 
-# test = Speaker.new("test")
-connections = Hash.new()
-
-
-File.open('complex_input.txt', 'r') do |f1|  
+File.open('sample_input.txt', 'r') do |f1|  
 
 	  while line = f1.gets
 		puts line
 
+		#Gets the speaker name
 		speaker = line[/\w+/]
+
+		#Gets the spoken to name(s)
 		speakee = line.scan(/\@\w+/)
-		#puts the speaker name
-	    #puts "speaker is #{speaker.class}"
-	    #puts the recipient
-	    #puts "speakee is #{speakee.class}"
+		speakee.map!{ |element| element.gsub(/@/, '') }
 
 	    	#Checks if speaker exists
 			if Speaker.list_speakers.include?(speaker)
@@ -32,9 +28,13 @@ File.open('complex_input.txt', 'r') do |f1|
 	end
 	
 	#Example code to get connections from first speaker
-	puts Speaker.list_connections(Speaker.list_speakers.first).inspect
+	example = Speaker.list_connections(Speaker.list_speakers.first).inspect
+	puts example
 
 	#Example code to get all speakers
-	puts Speaker.list_speakers.inspect
+	all_speakers = Speaker.list_speakers.inspect
 	#puts test.all_speakers
+	puts all_speakers
+	example.each {|example| puts all_speakers.index(example)}
+
 end
